781e7aac0b4f82061d3048bb7fed755193ef0439
Jan Gosmann <jan@hyper-world.de>
2020-04-20 19:52:24 +0200

    Initial commit

---
+++ FractalViewer_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef FRACTALVIEWER_PRIVATE_H
+#define FRACTALVIEWER_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"2.1.0.0"
+#define VER_MAJOR	2
+#define VER_MINOR	1
+#define VER_RELEASE	0
+#define VER_BUILD	26
+#define COMPANY_NAME	"www.hyper-world.de.vu"
+#define FILE_VERSION	"2.1"
+#define FILE_DESCRIPTION	"FractalViewer"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Copyright (C) 2005 Jan Gosmann"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"FractalViewer"
+#define PRODUCT_VERSION	"2.1"
+
+#endif /*FRACTALVIEWER_PRIVATE_H*/
